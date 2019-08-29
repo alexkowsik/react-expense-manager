5ead11fe0708d9834cfc6ceeb51fc630dd23f1c8
Alexander Kowsik <alexander.kowsik@gmx.de>
2019-08-29 19:14:50 +0200

    added person cards & reload button

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -8,26 +8,58 @@ const Error = props => {
 
 export default class PersonCard extends React.Component {
     state = {
-        name: undefined,
-        value: undefined,
-        inputInvalid: true
+        button: 'Submit',
+        name: '',
+        value: '',
+        inputInvalid: false,
+        inputMade: false
     };
 
     render(props) {
         const submitMoney = e => {
-            this.setState({
-                amount: e.target.value,
-                inputInvalid: !this.state.inputInvalid
-            });
+            if (
+                !/^[a-zA-ZäöüÄÖÜß 1234567890]+$/.test(this.state.name) ||
+                this.state.name === '' ||
+                isNaN(this.state.value) ||
+                this.state.value === ''
+            ) {
+                this.setState({ inputInvalid: true });
+            } else {
+                this.setState({ inputMade: true, inputInvalid: false });
+            }
+
             console.log(this.state);
         };
 
-        return (
+        const handleNameChange = e => {
+            this.setState({ name: e.target.value });
+        };
+
+        const handleValueChange = e => {
+            this.setState({ value: e.target.value });
+        };
+
+        return this.state.inputMade ? (
             <div className='person-card'>
-                <input type='name' value={this.state.name} placeholder='Person Name' />
-                <input type='number' value={this.state.value} placeholder='Value' />
+                <h2 className='person-card-name'>Name: {this.state.name}</h2>
+                <h2 className='person-card-value'>Paid: {this.state.value}</h2>
+            </div>
+        ) : (
+            <div className='person-card'>
+                <input
+                    type='name'
+                    value={this.state.name}
+                    placeholder='Person Name'
+                    onChange={handleNameChange}
+                />
+                <input
+                    type='number'
+                    value={this.state.value}
+                    placeholder='Paid amount'
+                    onChange={handleValueChange}
+                />
                 <Error error={this.state.inputInvalid} />
-                <button onClick={submitMoney}>Submit</button>
+                <button onClick={submitMoney}>{this.state.button}</button>
             </div>
         );
     }
